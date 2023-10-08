54c80573a88d61671fec240c62eac4322e5f2e78
Gustavo Bruno <gefgu12@gmail.com>
2023-10-08 11:12:20 -0300

    feat: initial splash screen

--- app-marinho/app/index.js
+++ app-marinho/app/index.js
@@ -1,11 +1,33 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import {Stack, useRouter} from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BackgroundColors } from "../assets/colors";
 
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            <Stack.Screen options={{headerShadowVisible: false, headerTitle: "", headerStyle: {backgroundColor: BackgroundColors.dark}}}/>
+            <LinearGradient style={styles.container} colors={[BackgroundColors.dark, BackgroundColors.bright]}>
+                <Text style={styles.heading}>Bem vindo ao Aqualis</Text>
+            </LinearGradient>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    heading: {
+        textTransform: "uppercase",
+        color: "white",
+        fontSize: 24,
+    }
+})
 
 export default Home;
---
+++ app-marinho/assets/colors.js
@@ -0,0 +1,6 @@
+const BackgroundColors = {
+    bright: "#06A0C4",
+    dark: "#040A2D"
+}
+
+export {BackgroundColors}
