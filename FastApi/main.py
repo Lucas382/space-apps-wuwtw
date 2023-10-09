c762d7a685cd36550f0672a8aa74e9c0279a127c
lucas <lucasalves382@hotmail.com>
2023-10-08 21:09:11 -0300

    ADD: adicionado rota para buscar informações sobre animais marinhos

--- FastApi/main.py
+++ FastApi/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from typing import Annotated
 import uvicorn
+from starlette.responses import JSONResponse
 
 from FastApi.src.infrastructure.data.db_context.postgre_sql_context import engine, SessionLocal
 from FastApi.src.application.api_models.question_choices_api_models import QuestionBase
@@ -24,9 +25,27 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def remove_left_spaces(df):
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    return df
+
+@app.get("/animais_marinhos/")
+async def get_sea_animals():
+    sea_animals_path = 'FastApi/src/infrastructure/data/repositories/animais_marinhos.xlsx'
+
+    df_sea_animals = pd.read_excel(sea_animals_path)
+    df_sea_animals = df_sea_animals.fillna('')
+
+    df_sea_animals = remove_left_spaces(df_sea_animals)
+
+    sea_animals_data = df_sea_animals.to_dict(orient='records')
+
+    return JSONResponse(content=sea_animals_data)
+
+
 @app.get("/municipios/{nome_municipio}")
 async def get_otto_code_from_name(name_of_city: str):
-    file_path_city = 'FastApi/src/infrastructure/data/repositories/bacias_nivel_4_municipios.xlsx'
+    file_path_city = 'FastApi/src/infrastructure/data/repositories/bacias_nivel_4_municipios_obsolete.xlsx'
     file_path_river_basin = 'FastApi/src/infrastructure/data/repositories/bacias_nivel_4.xlsx'
 
     df_city = pd.read_excel(file_path_city)
@@ -57,7 +76,7 @@ async def get_otto_code_from_name(name_of_city: str):
         if cod and int(cod) in list_cod_otto:
             print(int(cod) , list_cod_otto)
             result_objects.append({
-                'código Otto': int(cod) ,
+                'código Otto': int(cod),
                 'nome da bacia hidrográfica': watersheds_name[i],
                 'nome do curso principal': main_course[i],
                 'principais afluentes': main_tributary_rivers[i],
