7c26adefedfc81b4f220adca4bc7f0906242c71f
lucas <lucasalves382@hotmail.com>
2023-10-08 21:35:06 -0300

    REMOVE: Limpando arquivos

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,17 +1,14 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from typing import Annotated
 import uvicorn
 from starlette.responses import JSONResponse
 
 from FastApi.src.infrastructure.data.db_context.postgre_sql_context import engine, SessionLocal
-from FastApi.src.application.api_models.question_choices_api_models import QuestionBase
-from FastApi.src.domain.models import question_choices_repository_models as models
 
 import pandas as pd
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
 
 def get_db():
@@ -85,35 +82,6 @@ async def get_otto_code_from_name(name_of_city: str):
             })
 
     return result_objects
-
-
-@app.get("/questions/{question_id}")
-async def read_question(question_id: int, db: db_dependency):
-    result = db.query(models.Questions).filter(models.Questions.id == question_id).first()
-    if not result:
-        raise HTTPException(status_code=404, detail='Question not found')
-    return result
-
-
-@app.get('/choices/{question_id}')
-async def read_choices(question_id: int, db:db_dependency):
-    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
-    if not result:
-        raise HTTPException(status_code=404, detail='Choices not found')
-    return result
-
-
-@app.post("/questions/")
-async def create_questions(question: QuestionBase, db: db_dependency):
-    db_question = models.Questions(question_text=question.question_text)
-    db.add(db_question)
-    db.commit()
-    db.refresh(db_question)
-
-    for choice in question.choices:
-        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
-        db.add(db_choice)
-    db.commit()
 
 
 if __name__ == "__main__":
